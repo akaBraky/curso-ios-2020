b951824ddd3fb122605e37b6707479c1e5574085
SamArtGS <samuelarturo@icloud.com>
2019-12-07 13:52:49 -0600

    feat: Modified Playground

--- MyFirstPlayground.playground/Contents.swift
+++ MyFirstPlayground.playground/Contents.swift
@@ -1,3 +1,27 @@
 import UIKit
 
 var str = "Hello, playground"
+
+class Album {
+    var nombre: String
+    
+    init(nombre: String) {
+        self.nombre = nombre
+    }
+    func getPerformance() -> String {
+        return "The album \(nombre) sold lots"
+    }
+}
+
+class StudioAlbum: Album {
+    var studio: String
+    
+    init(nombre: String, studio: String) {
+        self.studio = studio
+        super.init(nombre: nombre)
+    }
+    
+    override func getPerformance() -> String {
+        return "The studio album \(nombre) sold lots"
+    }
+}
