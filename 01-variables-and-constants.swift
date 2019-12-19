78ae7745c56d1c145ceb4d16d3c08bcd773efacf
rhofp <rhodfra@gmail.com>
2019-12-19 13:21:11 -0600

    feat: Added variables and constants exercise

---
+++ 01-variables-and-constants.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+/* Ejemplo 1 */
+
+var name = "Rodrigo"
+name = "Francisco"
+
+/* Ejemplo 2
+ * Prueba negativa: debe fallar
+*/
+let name = "Tim McGraw"
+name = "Romeo"
+
+/* Ejemplo 3
+ * Prueba negativa: debe fallar
+*/
+var name = "Rodrigo"
+var name = "Francisco"
