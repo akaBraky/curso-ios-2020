d7b5a8671b4df32c22289c90a682eca5d60c3863
gabrielos307 <gabrielos307@gmail.com>
2020-01-12 18:28:16 -0600

    Tarea02/AlejandroHerrera

---
+++ IMC.swift
@@ -0,0 +1,11 @@
+print("Hola soy una calculadora de indice de masa corporal")
+var nombre = "Alejandro"
+var edad = 21
+var indice: Float
+var masa = 70
+var altura = 1.75
+var indiceSal = 22.5
+
+indice = (masa)/(altura*altura)
+
+print("\(nombre) tu indice de masa corporales es: \indice y tu edad es: \(edad), tu indice de masa corporal debe ser de: \(indiceSal)")
