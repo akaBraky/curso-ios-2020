26f163cf010c38843489525673735ba556efa81e
fernando <fernando.patricio.maceda@gmail.com>
2020-02-22 11:34:15 -0600

    feat: Homework

---
+++ Calculadora.swift
@@ -0,0 +1,43 @@
+﻿import Foundation
+
+func suma(_ a: Int, _ b: Int) -> Int {
+    return a + b
+}
+
+func resta(_ a: Int, _ b: Int) -> Int {
+    return a - b
+}
+
+func producto(_ a: Int, _ b: Int) -> Int {
+    return a * b
+}
+
+func division(_ a: Int, _ b: Int) -> Int {
+    return a / b
+}
+
+print("Ingresa el primer numero")
+var numeroUno = readLine()!
+var uno = Int(numeroUno)!
+print("Ingresa el segundo numero")
+var numeroDos = readLine()!
+var dos = Int(numeroDos)!
+print("¿Qué operacion deseas hacer?")
+print("+,-,*,/")
+var operacion = readLine()!
+switch operacion{
+    case "+":
+        print("La suma es: ",suma(uno,dos))
+    case "-":
+        print("La resta es: ",resta(uno,dos))
+    case "*":
+        print("La multiplicacion es: ",producto(uno,dos))
+    case "/":
+        print("La division es: ",division(uno,dos))
+    default:
+        print("Operacion no valida")
+}
+
+
+
+
